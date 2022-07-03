a6dc32d23c47c06222e58c251d188c762912d84b
ZhouSusan <szhou089@gmail.com>
2022-07-02 21:27:12 -0700

    fixed session error && tested databse

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CSharpWeddingPlanner.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace CSharpWeddingPlanner.Controllers
 {
@@ -27,68 +28,68 @@ namespace CSharpWeddingPlanner.Controllers
             return View("Index");
         }
 
-        // [HttpPost("register")]
-        // public IActionResult Register(User newUser)
-        // {
-        //     if (ModelState.IsValid) 
-        //     {
-        //         if (db.Users.Any(u=> u.Email == newUser.Email)) 
-        //         {
-        //             ModelState.AddModelError("Email", "Email is already in use");
-        //             return View("Index");
-        //         }
-        //         PasswordHasher<User> Hasher =  new PasswordHasher<User>();
-        //         //Hash the password only after verifying that everything else is good to go
-        //         newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
-        //         db.Add(newUser);
-        //         db.SaveChanges();
-        //         HttpContext.Session.SetInt32("UserId", newUser.UserId);
-        //         return RedirectToAction("Dashboard");
-        //     }
+        [HttpPost("register")]
+        public IActionResult Register(User newUser)
+        {
+            if (ModelState.IsValid) 
+            {
+                if (db.Users.Any(u=> u.Email == newUser.Email)) 
+                {
+                    ModelState.AddModelError("Email", "Email is already in use");
+                    return View("Index");
+                }
+                PasswordHasher<User> Hasher =  new PasswordHasher<User>();
+                //Hash the password only after verifying that everything else is good to go
+                newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+                db.Add(newUser);
+                db.SaveChanges();
+                HttpContext.Session.SetInt32("UserId", newUser.UserId);
+                return RedirectToAction("Dashboard");
+            }
 
-        //     return View("Index");
-        // }
+            return View("Index");
+        }
 
-        // [HttpPost("loginUser")]
-        // public IActionResult LoginUser(LogUser logUser)
-        // {
-        //     if (ModelState.IsValid)
-        //     {
-        //         User userindb = db.Users.FirstOrDefault(u => u.Email == logUser.LoginEmail);
-        //         if (userindb == null)
-        //         {
-        //             ModelState.AddModelError("LoginEmail", "Invalid login attempt");
-        //             return View("Index");
-        //         }
-        //         //check if password is correct
-        //         PasswordHasher<LogUser> Hasher = new PasswordHasher<LogUser>();
-        //         PasswordVerificationResult result = Hasher.VerifyHashedPassword(logUser, userindb.Password, logUser.LoginPassword); 
-        //         //When the vertifcation runs, it will passed 1(successfully) or 0(password is incorrect)
-        //         if (result == 0)
-        //         {
-        //             ModelState.AddModelError("LoginEmail", "Invalid login attempt");
-        //             return View("Index");
-        //         }
+        [HttpPost("loginUser")]
+        public IActionResult LoginUser(LogUser logUser)
+        {
+            if (ModelState.IsValid)
+            {
+                User userindb = db.Users.FirstOrDefault(u => u.Email == logUser.LoginEmail);
+                if (userindb == null)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid login attempt");
+                    return View("Index");
+                }
+                //check if password is correct
+                PasswordHasher<LogUser> Hasher = new PasswordHasher<LogUser>();
+                PasswordVerificationResult result = Hasher.VerifyHashedPassword(logUser, userindb.Password, logUser.LoginPassword); 
+                //When the vertifcation runs, it will passed 1(successfully) or 0(password is incorrect)
+                if (result == 0)
+                {
+                    ModelState.AddModelError("LoginEmail", "Invalid login attempt");
+                    return View("Index");
+                }
 
-        //         HttpContext.Session.SetInt32("UserId", userindb.UserId);
-        //         return RedirectToAction("Dashboard");
-        //     }
-        //     return View("Index");
-        // }
+                HttpContext.Session.SetInt32("UserId", userindb.UserId);
+                return RedirectToAction("Dashboard");
+            }
+            return View("Index");
+        }
 
 
-        // [HttpGet("/Dashboard")]
-        // public IActionResult Dashboard()
-        // {
-        //         if (HttpContext.Session.GetInt32("UserId") == null)
-        //         {
-        //             return RedirectToAction("Index");
-        //         }
-        //         int ? userId = HttpContext.Session.GetInt32("UserId");
-        //         List<Wedding> AllWeddings = db.Weddings.Include(w => w.GuestReponses).OrderBy(u => u.Date).ToList();
-        //         ViewBag.UserId = (int)userId;
-        //         return View("Dashboard", AllWeddings);
-        // }
+        [HttpGet("/Dashboard")]
+        public IActionResult Dashboard()
+        {
+                if (HttpContext.Session.GetInt32("UserId") == null)
+                {
+                    return RedirectToAction("Index");
+                }
+                int ? userId = HttpContext.Session.GetInt32("UserId");
+                List<Wedding> AllWeddings = db.Weddings.Include(w => w.GuestReponses).OrderBy(u => u.Date).ToList();
+                ViewBag.UserId = (int)userId;
+                return View("Dashboard", AllWeddings);
+        }
 
         // [HttpGet("/NewWedding")]
         // public IActionResult NewWedding()
